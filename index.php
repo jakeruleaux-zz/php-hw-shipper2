ddb4266e45c8a5a0886d898274db5f1105be7a82
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-25 15:18:11 -0700

    builds html and php forms

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    $input_weight = $_GET["weight"];
+    $input_distance = $_GET["distance"];
+    $shipping_price = calculateShippingPrice($input_weight, $input_distance);
+
+    function calculateShippingPrice($distance, $weight)
+    {
+      $price = $weight / 20 + $distance / 20;
+      return $price;
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shipping Calculator</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Price Calculated!</h1>
+        <h3>Your package weighs: <?php echo $input_weight . " lbs"; ?> </h3>
+        <h3>Your package is traveling: <?php echo $input_distance . " miles"; ?> </h3>
+        <h3>The cost will be: $<?php echo $shipping_price; ?> </h3>
+    </div>
+</body>
+</html>
